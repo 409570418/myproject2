fdd20560638c585a5f64ae6c319b2935f85184a5
409570418 <zzz1230626@gmail.com>
2022-04-21 22:30:38 +0800

    work

---
+++ src/main/java/sales/SalesTester.java
@@ -0,0 +1,26 @@
+package sales;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SalesTester {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(800);
+        list.add(1200);
+
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("001", 1200));
+        customers.add(new Customer("002", 800));
+        customers.add(new SilverCustomer("003", 2000));
+        customers.add(new Customer("006", 500));
+        customers.add(new GoldenCustomer("007", 900));
+        for (int i = 0; i < 5; i++) {
+            customers.get(i).print();
+        }
+        for (Customer c : customers) {
+            c.print();
+        }
+
+        //0001  1200    1100
+    }
+}
---
+++ src/main/java/sales/SilverCustomer.java
@@ -0,0 +1,17 @@
+package sales;
+
+public class SilverCustomer extends Customer {
+    public SilverCustomer(String id, int amount) {
+        super(id, amount);
+    }
+
+    public int getReturnMoney() {
+        return (int)(amount*0.1f);
+    }
+
+    @Override
+    public void print() {
+        System.out.println(id + "\t" + amount +
+                "\t" + (amount-getOffMoney())+"("+getReturnMoney()+")");
+    }
+}
